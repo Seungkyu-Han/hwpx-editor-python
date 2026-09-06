a828af248bee52f7f6146208a8def73155598291
Seungkyu-Han <trust1204@gmail.com>
2026-09-06 16:39:38 +0900

    chore: add Lang enum to hwpx_editor module

---
+++ hwpx_editor/enums/lang.py
@@ -0,0 +1,10 @@
+from enum import StrEnum
+
+class Lang(StrEnum):
+    HANGUL = "HANGUL"
+    LATIN = "LATIN"
+    HANJA = "HANJA"
+    JAPANESE = "JAPANESE"
+    OTHER = "OTHER"
+    SYMBOL = "SYMBOL"
+    USER = "USER"
